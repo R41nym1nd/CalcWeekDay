5a35d03e17334616b251c456256f63d6149aa9a4
Videla G. Tobías <tobiasvidelaug14@gmail.com>
2023-01-05 09:49:27 -0300

    added a new feature (press Enter to click button)

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const days = {
 };
 const GetDate = () => {
     let day = document.getElementById('day-n').value; // get day (number=1-31)
-    let month = document.getElementById('month-sigla').value; // get month (number=00-11)
+    let month = document.getElementById('month-n').value; // get month (number=00-11)
     let year = [document.getElementById('year-n').value]; // get year (object)
     year = year.toString(); // convert year to a processable value for the Date() function
     
@@ -26,3 +26,9 @@ const CalculateWeekDay = () => {
 
     return day;
 }
+document.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        document.getElementById("calc").click();
+    }
+});
