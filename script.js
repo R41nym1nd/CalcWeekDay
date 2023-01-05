50430623ce254545ef0ab097ed465ea0b9bdf45d
Videla G. Tobías <tobiasvidelaug14@gmail.com>
2023-01-05 09:02:51 -0300

    Modified the CalcWeekDay function (Spread operator added in the new Date())

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ const days = {
     6: "Saturday",
 };
 
-function CalculateWeekDay(){
+const CalculateWeekDay = () => {
     let day = document.getElementById('day-n').value; // get day (number=1-31)
     let month = document.getElementById('month-sigla').value; // get month (number=00-11)
     let year = [document.getElementById('year-n').value]; // get year (object)
     year = year.toString(); // convert year to a processable value for the Date() function
 
-    let weekDay = new Date(year, month, day); // get the date with the values above
+    let date = [year, month, day]; // assing the values to an array
+
+    let weekDay = new Date(...date); // get the date with spread operator
 
     return document.getElementById('result').innerHTML = days[weekDay.getDay()]; // calculate and obtain the week day
 }
